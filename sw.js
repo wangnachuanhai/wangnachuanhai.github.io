19e4b8889e0a79dd2ae76a5a8f4d09d0b91ba86a
wanghai <wanghai_huaxi@stu.scu.edu.cn>
2025-05-30 22:27:05 +0800

    initialize project configuration files

--- sw.js
+++ sw.js
@@ -23,7 +23,7 @@ const PRECACHE_LIST = [
   "./img/avatar-wh.jpg",
   "./img/home-bg.jpg",
   "./img/404-bg.jpg",
-  "./css/hux-blog.min.css",
+  "./css/hai-blog.min.css",
   "./css/bootstrap.min.css"
   // "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css",
   // "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/fonts/fontawesome-webfont.woff2?v=4.6.3",
